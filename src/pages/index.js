2ee53120f24b3bed8a1c63b44cec6d4878d0cb70
jhs512 <jangka512@gmail.com>
2022-06-01 09:55:21 +0900

    test와 test-space 제거, 레이아웃 꾸미기

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <title>홈</title>
-
-      <h1>홈 페이지</h1>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
 };
