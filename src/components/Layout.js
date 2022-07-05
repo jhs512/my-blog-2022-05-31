249d13fdc704cf5bf6c11ee491fc41a8f4ca3674
jhs512 <jangka512@gmail.com>
2022-07-05 17:21:44 +0900

    푸터 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,9 @@ import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 
 import {
+  layout,
   container,
+  footer,
   heading,
   navLinks,
   navLinkItem,
@@ -23,37 +25,40 @@ function Layout({ pageTitle, children }) {
   `);
 
   return (
-    <div className={container}>
-      <title>
-        {pageTitle} | {data.site.siteMetadata.title}
-      </title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/blog">
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={layout}>
+      <div className={container}>
+        <title>
+          {pageTitle} | {data.site.siteMetadata.title}
+        </title>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/about">
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/blog">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
 
-      <div>Copyright All Rights</div>
+        <div>Copyright All Rights</div>
+      </div>
+      <footer className={footer}>푸터</footer>
     </div>
   );
 }
